1b263bd0eac610c2b8a4617478c8a92ce8d6dc5e
unknown <budiarto.salim91@gmail.com>
2021-06-04 22:43:21 +0700

    added structure for the migration queries, with 2 sample query

---
+++ src/migrations/tablequeries/tables/items_init.ts
@@ -0,0 +1,16 @@
+export const items_init =
+  `CREATE TABLE IF NOT EXISTS "items" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    type VARCHAR(50),
+    effect VARCHAR(255),
+    effect_power INTEGER,
+    base_price INTEGER,
+    equip_stat_hp INTEGER,
+    equip_stat_atk INTEGER,
+    equip_stat_spd INTEGER,
+    equip_stat_hit INTEGER,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);`;
