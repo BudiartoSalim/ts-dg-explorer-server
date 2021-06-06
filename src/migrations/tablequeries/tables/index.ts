83a06eb860a5e4bbb187558e0c96d3d5ec625f29
unknown <budiarto.salim91@gmail.com>
2021-06-06 21:12:43 +0700

    added status effects and the conjunction table in erd

--- src/migrations/tablequeries/tables/index.ts
+++ src/migrations/tablequeries/tables/index.ts
@@ -1,4 +1,5 @@
 import { players_init } from './players_init';
 import { items_init } from './items_init';
 import { classes_init } from './classes_init';
-export { players_init, items_init, classes_init };
+import { skills_init } from './skills_init';
+export { players_init, items_init, classes_init, skills_init };
