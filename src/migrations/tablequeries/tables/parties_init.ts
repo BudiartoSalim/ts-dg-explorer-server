1ac799ce25d6ae0a803453e13c2526909b8018f4
unknown <budiarto.salim91@gmail.com>
2021-06-07 20:26:59 +0700

    added parties and unit status effects conjunction table migration

---
+++ src/migrations/tablequeries/tables/parties_init.ts
@@ -0,0 +1,12 @@
+
+export const parties_init =
+  `CREATE TABLE IF NOT EXISTS "parties" (
+    id SERIAL PRIMARY KEY,
+    player_id INTEGER,
+    first_unit_id INTEGER,
+    second_unit_id INTEGER,
+    third_unit_id INTEGER,
+    fourth_unit_id INTEGER,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);`;
