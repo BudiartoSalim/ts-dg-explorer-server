4e531eea676f207de339549947118402eaf56dbb
unknown <budiarto.salim91@gmail.com>
2021-06-13 15:02:04 +0700

    constraints update: fixed equips fk queries, added parties and unit_status_effects fks

---
+++ src/migrations/tablequeries/constraints/unit_status_effects_fk.ts
@@ -0,0 +1,12 @@
+export const unit_status_effects_fk_init =
+  `ALTER TABLE "unit_status_effects" (
+    ADD CONSTRAINT fk_affected_unit_id
+    FOREIGN KEY (unit_id)
+      REFERENCES "units"(id)
+      ON DELETE SET NULL,
+    ADD CONSTRAINT fk_active_effect_id
+    FOREIGN KEY (status_effect_id)
+      REFERENCES "status_effects"(id)
+      ON DELETE SET NULL,
+);
+`;
