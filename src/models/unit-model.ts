b354091e6ea4eabc61e0ca0a07067410dd562e64
unknown <budiarto.salim91@gmail.com>
2021-08-02 01:11:38 +0700

    hire unit method in model done, not implemented in controller yet. need to refactor auth abit.

--- src/models/unit-model.ts
+++ src/models/unit-model.ts
@@ -20,18 +20,63 @@ export default class Unit {
   static async decodeUnitJwt(unitJwt: string): Promise<IUnit> {
     try {
       const payload = jwt.verify(unitJwt, process.env.UNIT_SECRET as string);
-
       return payload as IUnit;
     } catch (err) {
       throw 'invalid-unit-token';
     }
   }
 
-  static async hireUnit(playerId: number, unit: IUnit) {
+  static async hireUnit(playerId: number, unit: IUnit): Promise<IUnit> {
     const client = await pool.connect();
     try {
+      await client.query('BEGIN;');
+      const insertEquipSheetQueryResult = await client.query(
+        `INSERT INTO equips
+          (first_skill_id, second_skill_id, main_weapon_id, sub_weapon_id, armor_id, accessory_id,
+          first_consumable_id, second_consumable_id, created_at, updated_at)
+        VALUES(null, null, null, null, null, null, null, null, NOW(), NOW())
+        RETURNING *;`
+      );
+      const newUnitEquipSheet = insertEquipSheetQueryResult.rows[0];
+
+      const insertUnitQueryResult = await client.query(
+        `INSERT INTO 
+        units(player_id, name, gender, level, level_cap, current_xp, next_xp, max_hp, current_hp,
+          max_energy, current_energy, base_atk, base_def, base_spd, base_hit, class_id, equip_id, created_at, updated_at) 
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17, NOW(), NOW())
+        RETURNING *;
+      `,
+        [playerId, unit.name, unit.gender, unit.level, unit.levelCap, unit.currentXp, unit.nextXp, unit.maxHp, unit.currentHp,
+          unit.maxEnergy, unit.currentEnergy, unit.baseAtk, unit.baseDef, unit.baseSpd, unit.baseHit, unit.class,
+          newUnitEquipSheet.id
+        ]
+      );
+      await client.query('COMMIT;');
+
+      const newUnit = insertUnitQueryResult.rows[0];
+      return {
+        id: newUnit.id,
+        playerId: newUnit.player_id,
+        name: newUnit.name,
+        gender: newUnit.gender,
+        level: newUnit.level,
+        levelCap: newUnit.level_cap,
+        currentXp: newUnit.current_xp,
+        nextXp: newUnit.next_xp,
+        maxHp: newUnit.max_hp,
+        currentHp: newUnit.current_hp,
+        maxEnergy: newUnit.max_energy,
+        currentEnergy: newUnit.current_energy,
+        baseAtk: newUnit.base_atk,
+        baseDef: newUnit.base_def,
+        baseSpd: newUnit.base_spd,
+        baseHit: newUnit.base_hit,
+        class: newUnit.class_id,
+        equips: newUnit.equip_id
+      }
 
     } catch (err) {
+      await client.query('ROLLBACK');
       throw err;
     } finally {
       client.release();
